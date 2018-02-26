fa7326629312937a9c78ecb5cc94e8e06c9b2997
Alexandre Roux Tekartik <alex@tekartik.com>
2018-02-27 00:42:12 +0100

    test: on 2.0.0-dev30

--- tool/travis.sh
+++ tool/travis.sh
@@ -3,11 +3,7 @@
 # Fast fail the script on failures.
 set -e
 
-dartanalyzer --fatal-warnings \
-  lib/sembast.dart \
-  lib/sembast_io.dart \
-  lib/sembast_memory.dart \
-  lib/utils/sembast_import_export.dart \
+dartanalyzer --fatal-warnings .
 
 pub run test -p vm -j 1
 pub run test -p firefox -j 1
