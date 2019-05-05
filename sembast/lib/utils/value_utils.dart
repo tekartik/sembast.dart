b1e0823835e55bfa591567df550ce9e6e98f4433
alex <alex@tekartik.com>
2019-05-05 16:41:14 +0200

    feat: add cloneMap

--- sembast/lib/utils/value_utils.dart
+++ sembast/lib/utils/value_utils.dart
@@ -1,19 +1,28 @@
 import 'package:sembast/src/utils.dart';
 
 /// @deprecated v2
+@deprecated
 bool lessThan(dynamic value1, dynamic value2) {
   var cmp = compareValue(value1, value2);
   return cmp != null && cmp < 0;
 }
 
 /// @deprecated v2
+@deprecated
 bool greaterThan(dynamic value1, dynamic value2) {
   var cmp = compareValue(value1, value2);
   return cmp != null && cmp > 0;
 }
 
 /// @deprecated v2
+@deprecated
 bool equals(dynamic value1, dynamic value2) {
   var cmp = compareValue(value1, value2);
   return cmp == 0;
 }
+
+/// Clone a value to make it writable, typically a map
+///
+/// This should be used to create a writable object that can be modified
+Map<String, dynamic> cloneMap(Map<String, dynamic> value) =>
+    cloneValue(value) as Map<String, dynamic>;
--- sembast/test/compat/value_utils_test.dart
+++ sembast/test/compat/value_utils_test.dart
@@ -8,6 +8,44 @@ import 'test_common.dart';
 
 void main() {
   group('value_utils', () {
+    test('cloneMap', () {
+      var existing = <String, dynamic>{
+        'test': 1,
+        'nested': {
+          'sub': 2,
+          'list': [
+            {'n': 1},
+            2
+          ]
+        }
+      };
+      var cloned = utils.cloneMap(existing);
+      expect(cloned, existing);
+      existing['test'] = 3;
+      (existing['nested'] as Map)['sub'] = 4;
+      (((existing['nested'] as Map)['list'] as List)[0] as Map)['n'] = 5;
+      // Make sure chaging the existing does not change the clone
+      expect(existing, {
+        'test': 3,
+        'nested': {
+          'sub': 4,
+          'list': [
+            {'n': 5},
+            2
+          ]
+        }
+      });
+      expect(cloned, {
+        'test': 1,
+        'nested': {
+          'sub': 2,
+          'list': [
+            {'n': 1},
+            2
+          ]
+        }
+      });
+    });
     test('compare', () {
       expect(compareValue('1', '2'), Comparable.compare('1', '2'));
       expect(compareValue(1, 2), Comparable.compare(1, 2));
@@ -18,20 +56,28 @@ void main() {
     });
 
     test('equals', () {
+      // ignore: deprecated_member_use_from_same_package
       expect(utils.equals(0, 0), isTrue);
+      // ignore: deprecated_member_use_from_same_package
       expect(utils.equals(0, 1), isFalse);
 
       // array
+      // ignore: deprecated_member_use_from_same_package
       expect(utils.equals([0], [0]), isTrue);
     });
 
     test('lessThen', () {
+      // ignore: deprecated_member_use_from_same_package
       expect(utils.lessThan(0, 1), isTrue);
+      // ignore: deprecated_member_use_from_same_package
       expect(utils.lessThan(0, 0), isFalse);
 
       // array
+      // ignore: deprecated_member_use_from_same_package
       expect(utils.lessThan([0], [1]), isTrue);
+      // ignore: deprecated_member_use_from_same_package
       expect(utils.lessThan([0], [0, 0]), isTrue);
+      // ignore: deprecated_member_use_from_same_package
       expect(utils.lessThan([0], [0]), isFalse);
     });
   });
