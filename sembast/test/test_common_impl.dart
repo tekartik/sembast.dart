2b2eb9e14343f24efad8da88b639ab4318312a8e
alex <alex@tekartik.com>
2019-07-17 18:38:13 +0200

    test: remove print

--- sembast/test/test_common_impl.dart
+++ sembast/test/test_common_impl.dart
@@ -1,10 +1,6 @@
 // basically same as the io runner but with extra output
 import 'package:sembast/src/database_impl.dart';
 
-// export 'package:dev_test/test.dart';
-// ignore: deprecated_member_use_from_same_package
-export 'package:sembast/src/dev_utils.dart' show devPrint;
-
 DatabaseExportStat getDatabaseExportStat(SembastDatabase db) {
   return DatabaseExportStat.fromJson(db.toJson()["exportStat"] as Map);
 }
