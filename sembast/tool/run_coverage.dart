95fe0cddc3de8f799d6adc0de833a19d863791e2
alex <alex@tekartik.com>
2019-10-28 19:50:41 +0100

    test: add coverage run

---
+++ sembast/tool/run_coverage.dart
@@ -0,0 +1,28 @@
+import 'package:process_run/shell.dart';
+import 'package:process_run/which.dart';
+
+Future main() async {
+  var shell = Shell();
+
+  var obsPort = 9292;
+  if (whichSync('coverage') == null) {
+    await shell.run('''
+
+pub global activate coverage
+
+''');
+  }
+
+  Future start = shell.run('''
+   dart --disable-service-auth-codes --enable-vm-service=$obsPort --pause-isolates-on-exit test/store_test.dart
+
+  ''');
+
+  await shell.run('''
+  pub global run coverage:collect_coverage --port=$obsPort --out=coverage/coverage.json --wait-paused --resume-isolates
+  
+  pub global run coverage:format_coverage --lcov --in=coverage/coverage.json --out=coverage/lcov.info --packages=.packages --report-on=lib
+
+  ''');
+  await start;
+}
