aae7909fdc26b2ed0d53c230e89bf0c3f0e55240
alex <alex@tekartik.com>
2020-02-11 19:46:15 +0100

    [web] example

---
+++ sembast_web/example/alt_store.dart
@@ -0,0 +1,5 @@
+import 'store.dart';
+
+Future main() async {
+  await run(dbName: 'sembast_web_example_alt_store', store: 'alt_data');
+}
--- sembast_web/example/record.dart
+++ sembast_web/example/record.dart
@@ -2,6 +2,8 @@ import 'dart:html';
 
 import 'package:sembast_web/sembast_web.dart';
 
+import 'common.dart';
+
 Database db;
 var factory = databaseFactoryWeb;
 var counterRecord = StoreRef<String, int>.main().record('counter');
@@ -24,30 +26,4 @@ Future main() async {
     write('deleting...');
     await counterRecord.delete(db);
   });
-}
-
-class OutBuffer {
-  int _maxLineCount;
-  List<String> lines = [];
-
-  OutBuffer(int maxLineCount) {
-    _maxLineCount = maxLineCount ?? 100;
-  }
-
-  void add(String text) {
-    lines.add(text);
-    while (lines.length > _maxLineCount) {
-      lines.removeAt(0);
-    }
-  }
-
-  @override
-  String toString() => lines.join('\n');
-}
-
-OutBuffer _outBuffer = OutBuffer(100);
-Element _output = document.getElementById('output');
-void write([Object message]) {
-  print(message);
-  _output.text = (_outBuffer..add('$message')).toString();
 }
