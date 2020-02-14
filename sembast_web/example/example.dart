3dc0ee69c0a3e7f0d3cf6ab180a0d43f2a821b15
alex <alex@tekartik.com>
2020-02-14 13:00:47 +0100

    [sembast_web] feat: add example

---
+++ sembast_web/example/example.dart
@@ -0,0 +1,23 @@
+import 'package:sembast_web/sembast_web.dart';
+
+Future main() async {
+  // Declare our store (records are mapd, ids are ints)
+  var store = intMapStoreFactory.store();
+  var factory = databaseFactoryWeb;
+
+  // Open the database
+  var db = await factory.openDatabase('test');
+
+  // Add a new record
+  var key =
+      await store.add(db, <String, dynamic>{'name': 'Table', 'price': 15});
+
+  // Read the record
+  var value = await store.record(key).get(db);
+
+  // Print the value
+  print(value);
+
+  // Close the database
+  await db.close();
+}
