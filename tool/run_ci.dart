d5aaba4d043f64ab2a7bda191b911262ad091012
alex <alex@tekartik.com>
2020-11-21 02:49:56 +0100

    ci: skip root folder

--- tool/run_ci.dart
+++ tool/run_ci.dart
@@ -6,7 +6,7 @@ Future main() async {
   var nnbdEnabled = dartVersion > Version(2, 12, 0, pre: '0');
   if (nnbdEnabled) {
     for (var dir in [
-      '.',
+      // '.',
       'sembast',
       //'sembast_test',
       //'sembast_web',
