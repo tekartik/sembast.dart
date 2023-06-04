62dc8b11944e8d26457137673632dc9d887cb6d6
Alexandre Roux <alex@tekartik.com>
2023-06-04 15:49:05 +0200

    feat: add SortOrder.custom

--- sembast/lib/src/sort_order_impl.dart
+++ sembast/lib/src/sort_order_impl.dart
@@ -1,24 +1,24 @@
 import 'package:sembast/sembast.dart';
 import 'package:sembast/src/boundary_impl.dart';
-import 'package:sembast/src/utils.dart';
+import 'package:sembast/src/utils.dart' as utils;
 
-/// Sort order implementation.
-class SembastSortOrder implements SortOrder {
+/// Base sort order implementation.
+abstract class SembastSortOrderBase<T extends Object?> implements SortOrder<T> {
   /// ascending.
   final bool ascending; // default true
-  /// field (key) name.
-  final String field;
 
   /// null last.
-  final bool nullLast; // default false
+  final bool nullLast;
 
-  ///
-  /// default is [ascending] = true, [nullLast] = false
-  ///
-  /// user withParam
-  SembastSortOrder(this.field, [bool? ascending, bool? nullLast])
-      : ascending = ascending != false,
-        nullLast = nullLast == true;
+  /// Base sort order implementation.
+  SembastSortOrderBase(this.ascending, this.nullLast); // default false
+
+  /// Compare 2 records in ascending order.
+  int compareAscending(RecordSnapshot record1, RecordSnapshot record2);
+
+  /// Compare with a boundary in ascending order.
+  int compareToBoundaryAscending(
+      RecordSnapshot record, Boundary boundary, int index);
 
   /// Compare 2 record.
   int compare(RecordSnapshot record1, RecordSnapshot record2) {
@@ -32,36 +32,8 @@ class SembastSortOrder implements SortOrder {
     return ascending ? result : -result;
   }
 
-  /// Compare a record to a snapshot.
-  int compareToSnapshotAscending(
-      RecordSnapshot record, RecordSnapshot snapshot) {
-    var value1 = record[field];
-    var value2 = snapshot[field];
-    return compareValueAscending(value1, value2);
-  }
-
-  /// Compare a record to a boundary in ascending order.
-  int compareToBoundaryAscending(
-      RecordSnapshot record, Boundary boundary, int index) {
-    final sembastBoundary = boundary as SembastBoundary;
-    if (sembastBoundary.values != null) {
-      var value = sembastBoundary.values![index];
-      return compareValueAscending(record[field], value);
-    } else if (sembastBoundary.snapshot != null) {
-      return compareToSnapshotAscending(record, sembastBoundary.snapshot!);
-    }
-    throw ArgumentError('either record or values must be provided');
-  }
-
-  /// Compare 2 records in ascending order.
-  int compareAscending(RecordSnapshot record1, RecordSnapshot record2) {
-    var value1 = record1[field];
-    var value2 = record2[field];
-    return compareValueAscending(value1, value2);
-  }
-
   /// Compare 2 values in ascending order.
-  int compareValueAscending(dynamic value1, dynamic value2) {
+  int compareValueAscending(Object? value1, Object? value2) {
     if (value1 == null) {
       if (value2 == null) {
         return 0;
@@ -81,6 +53,54 @@ class SembastSortOrder implements SortOrder {
     return compareValue(value1, value2);
   }
 
+  /// Compare 2 values. (overriden in custom sort order)
+  int compareValue(Object value1, Object value2) {
+    return utils.compareValue(value1, value2);
+  }
+}
+
+/// Sort order implementation.
+class SembastSortOrder<T extends Object?> extends SembastSortOrderBase<T> {
+  /// field (key) name.
+  final String field;
+
+  ///
+  /// default is [ascending] = true, [nullLast] = false
+  ///
+  /// user withParam
+  SembastSortOrder(this.field, [bool? ascending, bool? nullLast])
+      : super(ascending ?? true, nullLast ?? false);
+
+  /// Compare a record to a snapshot.
+  int compareToSnapshotAscending(
+      RecordSnapshot record, RecordSnapshot snapshot) {
+    var value1 = record[field];
+    var value2 = snapshot[field];
+    return compareValueAscending(value1, value2);
+  }
+
+  /// Compare a record to a boundary in ascending order.
+  @override
+  int compareToBoundaryAscending(
+      RecordSnapshot record, Boundary boundary, int index) {
+    final sembastBoundary = boundary as SembastBoundary;
+    if (sembastBoundary.values != null) {
+      var value = sembastBoundary.values![index];
+      return compareValueAscending(record[field], value);
+    } else if (sembastBoundary.snapshot != null) {
+      return compareToSnapshotAscending(record, sembastBoundary.snapshot!);
+    }
+    throw ArgumentError('either record or values must be provided');
+  }
+
+  /// Compare 2 records in ascending order.
+  @override
+  int compareAscending(RecordSnapshot record1, RecordSnapshot record2) {
+    var value1 = record1[field];
+    var value2 = record2[field];
+    return compareValueAscending(value1, value2);
+  }
+
   Map<String, Object?> _toDebugMap() {
     final map = <String, Object?>{
       field: ascending ? 'asc' : 'desc',
@@ -92,5 +112,26 @@ class SembastSortOrder implements SortOrder {
   @override
   String toString() {
     return _toDebugMap().toString();
+  }
+}
+
+/// Custom compare value function.
+typedef SembastCustomSortOrderCompareFunction<T> = int Function(
+    T value1, T value2);
+
+/// Custom sort order compare function.
+class SembastCustomSortOrder<T extends Object?> extends SembastSortOrder<T> {
+  final SembastCustomSortOrderCompareFunction<T> _compare;
+
+  /// Custom sort order compare function.
+  SembastCustomSortOrder(
+      String field, SembastCustomSortOrderCompareFunction<T> compare,
+      [bool? ascending, bool? nullLast])
+      : _compare = compare,
+        super(field, ascending, nullLast);
+
+  @override
+  int compareValue(Object? value1, Object? value2) {
+    return _compare(value1 as T, value2 as T);
   }
 }
