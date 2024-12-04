bb29e64eb5e7afa728e281d5d44072de1aae5166
alex <alex@tekartik.com>
2024-12-04 10:03:00 +0100

    Export json helpers from `utils/type_adapters.dart`

--- sembast/lib/utils/type_adapter.dart
+++ sembast/lib/utils/type_adapter.dart
@@ -1,3 +1,17 @@
 export 'package:sembast/src/json_encodable_codec.dart'
-    show JsonEncodableCodec, JsonEncodableDecoder, JsonEncodableEncoder;
-export 'package:sembast/src/type_adapter_impl.dart' show SembastTypeAdapter;
+    show
+        JsonEncodableCodec,
+        JsonEncodableDecoder,
+        JsonEncodableEncoder,
+        sembastDefaultJsonEncodableCodec;
+export 'package:sembast/src/sembast_codec.dart'
+    show
+        sembastCodecToJsonEncodable,
+        sembastCodecFromJsonEncodable,
+        sembastCodecDefault;
+export 'package:sembast/src/type_adapter_impl.dart'
+    show
+        SembastTypeAdapter,
+        sembastTimestampAdapter,
+        sembastBlobAdapter,
+        sembastDefaultTypeAdapters;
