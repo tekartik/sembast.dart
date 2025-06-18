cf0c4f60cdb346ad8a18962c3201f5b5768bebc7
Alexandre Roux <alex@tekartik.com>
2025-06-18 17:08:07 +0200

    web worker example minor updates

--- example/sembast_web_worker_exp/tool/build_and_serve_web.dart
+++ example/sembast_web_worker_exp/tool/build_and_serve_web.dart
@@ -1,18 +1,14 @@
-// ignore_for_file: avoid_print
+import 'dart:io';
 
-import 'package:dev_build/build_support.dart';
 import 'package:process_run/shell.dart';
 import 'package:tekartik_app_web_build/dhttpd.dart';
 
 Future<void> main(List<String> args) async {
-  await checkAndActivatePackage('dhttpd');
-  if (true) {
-    await run('''
+  await run('''
       dart pub get
       webdev build -o web:build
   ''');
-  }
   await dhttpdReady();
-  print('http://localhost:8080');
+  stdout.writeln('http://localhost:8080');
   await Shell(workingDirectory: 'build').run('dhttpd');
 }
